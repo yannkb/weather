8c47d26cbec415645678a8827baaf82bc25cfd19
Yann Bouchereau <yann.bchr@gmail.com>
2024-03-17 17:09:02 +1100

    📦 NEW: initial commit

--- importmap.php
+++ importmap.php
@@ -25,4 +25,17 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'htmx.org' => [
+        'version' => '1.9.11',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.3',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
 ];
---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class WeatherController extends AbstractController
+{
+    public function __construct(
+        private HttpClientInterface $client,
+    ) {
+    }
+
+    #[Route('/', name: 'app_weather')]
+    public function index(): Response
+    {
+        return $this->render('weather/index.html.twig');
+    }
+
+    #[Route('/search', name: 'app_weather_search')]
+    public function search(Request $request): Response
+    {
+        $accuWeatherApiKey = $this->getParameter('app.accuweather_api_key');
+        $search = $request->get('city');
+        $location = $this->client->request(
+            'GET',
+            'http://dataservice.accuweather.com/locations/v1/cities/search',
+            [
+                'query' => [
+                    'apikey' => $accuWeatherApiKey,
+                    'q' => $search
+                ]
+            ]
+        );
+
+        if ($location->getStatusCode() !== 200) {
+            // Return error
+        }
+
+        $currentCondition = $this->client->request(
+            'GET',
+            'http://dataservice.accuweather.com/currentconditions/v1/' . $location->toArray()[0]['Key'],
+            [
+                'query' => [
+                    'apikey' => $accuWeatherApiKey
+                ]
+            ]
+        );
+
+        if ($currentCondition->getStatusCode() !== 200) {
+            // Return error
+        }
+
+        $isNight = false;
+        $time = date('H', $currentCondition->toArray()[0]['LocalObservationDateTime']);
+        if ($time < 8 && $time > 20) {
+            $isNight = true;
+        }
+
+        $weather = [
+            'location' => $location->toArray()[0]['LocalizedName'],
+            'country' => $location->toArray()[0]['Country']['LocalizedName'],
+            'localObservationDateTime' => $currentCondition->toArray()[0]['LocalObservationDateTime'],
+            'weatherText' => $currentCondition->toArray()[0]['WeatherText'],
+            'temperature' => $currentCondition->toArray()[0]['Temperature']['Metric']['Value'] . '°' . $currentCondition->toArray()[0]['Temperature']['Metric']['Unit'],
+            'isNight' => $isNight,
+            // 'icon' => ,
+        ];
+
+        return $this->render('weather/_card.html.twig', [
+            'weather' => $weather
+        ]);
+    }
+}
